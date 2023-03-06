b810079673a25728d0517321e878382ec80d51fb
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-03-06 14:59:22 +0530

    Update __init__.py

--- InvadedRobot/helpers/__init__.py
+++ InvadedRobot/helpers/__init__.py
@@ -1,5 +1,5 @@
 import config
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-mongo = MongoClient(config.DB_URL)
+mongodb = MongoClient(config.DB_URL)
 dbname = mongo.MissKatyDB
